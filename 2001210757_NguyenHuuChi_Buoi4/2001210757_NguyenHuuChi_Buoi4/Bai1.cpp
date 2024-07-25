c34c4e35b58365efc37d8a669a4c084c154eae98
Chi <huuchitn@gmail.com>
2024-07-25 14:02:15 +0700

    Phan3 Bai3 Buoi 4

--- 2001210757_NguyenHuuChi_Buoi4/2001210757_NguyenHuuChi_Buoi4/Bai1.cpp
+++ 2001210757_NguyenHuuChi_Buoi4/2001210757_NguyenHuuChi_Buoi4/Bai1.cpp
@@ -1,29 +1,29 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
-
-bool isDigitString(const char* s) {
-    while (*s) {
-        if (!isdigit((unsigned char)*s)) {
-            return false;
-        }
-        s++;
-    }
-    return true;
-}
-
-int main() {
-    char s[100];
-
-    printf("Nhap chuoi: ");
-    scanf("%99s", s);
-
-    if (isDigitString(s)) {
-        printf("Chuoi '%s' chi chua ky so.\n", s);
-    }
-    else {
-        printf("Chuoi '%s' co chua ky tu khong phai ky so.\n", s);
-    }
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <ctype.h>
+//#include <stdbool.h>
+//
+//bool isDigitString(const char* s) {
+//    while (*s) {
+//        if (!isdigit((unsigned char)*s)) {
+//            return false;
+//        }
+//        s++;
+//    }
+//    return true;
+//}
+//
+//int main() {
+//    char s[100];
+//
+//    printf("Nhap chuoi: ");
+//    scanf("%99s", s);
+//
+//    if (isDigitString(s)) {
+//        printf("Chuoi '%s' chi chua ky so.\n", s);
+//    }
+//    else {
+//        printf("Chuoi '%s' co chua ky tu khong phai ky so.\n", s);
+//    }
+//
+//    return 0;
+//}
--- 2001210757_NguyenHuuChi_Buoi4/2001210757_NguyenHuuChi_Buoi4/Bai2.cpp
+++ 2001210757_NguyenHuuChi_Buoi4/2001210757_NguyenHuuChi_Buoi4/Bai2.cpp
@@ -1,36 +1,36 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
-
-void capitalizeWords(char* s) {
-    bool newWord = true;
-
-    while (*s) {
-        if (isspace((unsigned char)*s)) {
-            newWord = true;
-        }
-        else {
-            if (newWord) {
-                *s = toupper((unsigned char)*s);
-                newWord = false;
-            }
-            else {
-                *s = tolower((unsigned char)*s);
-            }
-        }
-        s++;
-    }
-}
-
-int main() {
-    char s[200];
-
-    printf("Nhap chuoi: ");
-    fgets(s, sizeof(s), stdin);
-
-    capitalizeWords(s);
-
-    printf("Chuoi sau khi bien doi: %s\n", s);
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <ctype.h>
+//#include <stdbool.h>
+//
+//void capitalizeWords(char* s) {
+//    bool newWord = true;
+//
+//    while (*s) {
+//        if (isspace((unsigned char)*s)) {
+//            newWord = true;
+//        }
+//        else {
+//            if (newWord) {
+//                *s = toupper((unsigned char)*s);
+//                newWord = false;
+//            }
+//            else {
+//                *s = tolower((unsigned char)*s);
+//            }
+//        }
+//        s++;
+//    }
+//}
+//
+//int main() {
+//    char s[200];
+//
+//    printf("Nhap chuoi: ");
+//    fgets(s, sizeof(s), stdin);
+//
+//    capitalizeWords(s);
+//
+//    printf("Chuoi sau khi bien doi: %s\n", s);
+//
+//    return 0;
+//}
